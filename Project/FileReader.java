21c1103c5888abc1caec22d738f5fba515ef2fad
Molnár Bence György <mlnr.bnc.gy@gmail.com>
2024-04-23 23:59:48 +0200

    A sorok beolvasásának logikája(tárgyak nélkül még), a fájlból olvasás még nem működik teljesen.

--- Project/FileReader.java
+++ Project/FileReader.java
@@ -26,7 +26,7 @@ public class FileReader {
                     i = readRooms(lines, i+1);
                     break;
                 case "AJTOK1":
-                    i = readDoors1(line, i+1);
+                    i = readDoors1(lines, i+1);
                     break;
                 case "AJTOK2":
                     i = readDoors2(lines, i+1);
@@ -52,7 +52,85 @@ public class FileReader {
 
             if(line.matches("\\d+")){
                 int capacity = Integer.parseInt(line);
-                controller.getMap().add(new Room(capacity));        //Room konstruktor capacity paraméterrel!!
+                controller.getMap().add(new Room("asd", false, false, capacity));
+            } else {
+                return i - 1;
+            }
+        }
+        return l.size();
+    }
+
+    public int readDoors1(List<String> l, int si){
+        for(int i = si; i < l.size(); i++){
+            String line = l.get(i).trim();
+
+            if(line.matches("\\d+")){
+                String[] rooms = line.split(" ");
+                int r1 = Integer.parseInt(rooms[0]);
+                int r2 = Integer.parseInt(rooms[1]);
+                controller.getMap().get(r1).addNeighbour(controller.getMap().get(r2), true);
+            } else {
+                return i - 1;
+            }
+        }
+        return l.size();
+    }
+
+    public int readDoors2(List<String> l, int si){
+        for(int i = si; i < l.size(); i++){
+            String line = l.get(i).trim();
+
+            if(line.matches("\\d+")){
+                String[] rooms = line.split(" ");
+                int r1 = Integer.parseInt(rooms[0]);
+                int r2 = Integer.parseInt(rooms[1]);
+                controller.getMap().get(r1).addNeighbour(controller.getMap().get(r2), false);
+            } else {
+                return i - 1;
+            }
+        }
+        return l.size();
+    }
+
+    public int readStudents(List<String> l, int si){
+        for(int i = si; i < l.size(); i++){
+            String line = l.get(i).trim();
+
+            if(line.matches("\\d+")){
+                int position = Integer.parseInt(line);
+                Student stud = new Student("asd");
+                controller.getMap().get(position).moveRoom(stud);
+                controller.getStudents().add(stud);
+            } else {
+                return i - 1;
+            }
+        }
+        return l.size();
+    }
+
+    public int readProfessors(List<String> l, int si){
+        for(int i = si; i < l.size(); i++){
+            String line = l.get(i).trim();
+
+            if(line.matches("\\d+")){
+                int position = Integer.parseInt(line);
+                Professor prof = new Professor("asd");
+                controller.getMap().get(position).moveRoom(prof);
+                controller.getProfessors().add(prof);
+            } else {
+                return i - 1;
+            }
+        }
+        return l.size();
+    }
+
+    public int readLogarlec(List<String> l, int si){
+        for(int i = si; i < l.size(); i++){
+            String line = l.get(i).trim();
+
+            if(line.matches("\\d+")){
+                Logarlec log = new Logarlec("asd");                        //szükséges a név a konstruktorba?
+                controller.getMap().get(i).addItem(log);
             } else {
                 return i - 1;
             }
