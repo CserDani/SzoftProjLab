b95c7ef05153a380244eadaccc0405c37607f28e
Cserhegyi Dániel <cserhegyidaniel@gmail.com>
2024-05-16 10:20:39 +0200

    Unnecessary imports removed in GameView

--- Project/GameView.java
+++ Project/GameView.java
@@ -1,8 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
